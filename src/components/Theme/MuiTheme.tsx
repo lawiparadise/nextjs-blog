88baea94fa7f1e2686d8606d68138bf75dac7ce7
lawiparadise <lawiparadise@gmail.com>
2023-08-23 14:39:26 +0900

    fin: cookie to change theme fin

--- src/components/Theme/MuiTheme.tsx
+++ src/components/Theme/MuiTheme.tsx
@@ -5,7 +5,7 @@ import { orange, amber, grey, deepOrange } from '@mui/material/colors'
 import { ThemeProvider as MuiThemeProvider } from '@mui/material'
 import React, { createContext, useEffect, useMemo, useState } from 'react'
 import CssBaseline from '@mui/material/CssBaseline';
-import { setThemeCookie } from '@/lib/cookieStore'
+import { changeThemeCookie } from '@/lib/cookieStore'
 
 // https://mui.com/material-ui/customization/dark-mode/
 export const ColorModeContext = createContext({
@@ -80,36 +80,17 @@ const getDesignTokens = (mode: PaletteMode) => ({
 })
 
 export default function ThemeProvider({ children, theme }: { children: React.ReactNode, theme: any }) {
-  console.log('theme', theme)
-
-  // todo useMediaQuery는 나중에 하기
-  // const t = theme ?? useMediaQuery('(prefers-color-scheme: dark)') ? 'dark' : 'light'
-  // console.log('useMediaQuery(\'(prefers-color-scheme: dark)\')', useMediaQuery('(prefers-color-scheme: dark)'))
-
   const [mode, setMode] = React.useState<'light' | 'dark'>(theme ?? 'dark');
-  console.log('mode', mode)
 
   const colorMode = useMemo(() => ({
     toggleColorMode: () => {
-      console.log('toggle')
-      setThemeCookie(mode === 'light' ? 'dark' : 'light').then()
-
-      setMode((prevMode) => (prevMode === 'light' ? 'dark' : 'light'))
+      // console.log('toggle')
+      changeThemeCookie().then((v)=>{
+        setMode(v)
+      })
     },
   }), [])
 
-  // dark mode from browser setting
-  // const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
-  // const toggleTheme = useMemo(() =>
-  //     createTheme({
-  //       palette: {
-  //         mode: prefersDarkMode ? 'dark' : 'light',
-  //       },
-  //     }),
-  //   [prefersDarkMode],
-  // )
-
-  // todo dark mode from cookie
   const toggleTheme = useMemo(() => createTheme(customTheme(mode)), [mode])
 
   return (
--- src/lib/cookieStore.tsx
+++ src/lib/cookieStore.tsx
@@ -2,8 +2,9 @@
 
 import { cookies } from 'next/headers'
 
-export async function setThemeCookie(mode: string) {
-  console.log('cookieStore', mode)
-  cookies().set('theme', mode)
-  return mode
+export async function changeThemeCookie() {
+  const t = cookies().get('theme')?.value === 'light' ? 'dark' : 'light'
+  // console.log('cookieStore', t)
+  cookies().set('theme', t)
+  return t
 }
