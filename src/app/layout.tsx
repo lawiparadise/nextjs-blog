443e30484ee7059e9e4f134e87d4c3c0ae7733aa
lawiparadise <lawiparadise@gmail.com>
2024-01-02 01:52:09 +0900

    test: meatadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,9 +4,15 @@ import { ReduxProvider } from '@/lib/redux/ReduxProvider'
 import { cookies } from 'next/headers'
 import MuiThemeProvider from '@/components/Theme/MuiThemeProvider'
 
+const title = process.env.NEXT_PUBLIC_BLOG_TITLE
+
 export const metadata: Metadata = {
-  title: 'home',
-  description: 'homepage',
+  title: title,
+  description: title+"dd",
+  openGraph: {
+    title: title,
+    description: "hi there",
+  },
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
